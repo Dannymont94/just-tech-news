5e22da126d7eb7b89f2522cc4df22bc09dc3c64b
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-08-26 17:54:23 -0400

    added post routes

--- models/index.js
+++ models/index.js
@@ -1,4 +1,15 @@
 const User = require('./User');
 const Post = require('./Post');
 
+// create associations
+// one user can make many posts
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
+
+// posts are made by only one user
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
 module.exports = { User, Post };
