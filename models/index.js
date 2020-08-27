dc7e42301acedb5268ca2a7aae4e63e5f8da8240
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-08-27 02:08:40 -0400

    added vote model and routes. get requests for all posts and posts by id now show vote count. get requests for user by id now show all posts and all upvoted posts.

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const Post = require('./Post');
+const Vote = require('./Vote');
 
 // create associations
 // one user can make many posts
@@ -12,4 +13,34 @@ Post.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Post };
+User.belongsToMany(Post, {
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'user_id'
+});
+
+Post.belongsToMany(User, {
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'post_id'
+});
+
+// By also creating one-to-many associations directly between these models, we can perform aggregated SQL functions between models
+// In this case, we'll see a total count of votes for a single post when queried
+Vote.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+Vote.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Vote, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Vote, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote };
