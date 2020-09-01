ac6daa53d0802fb38f1870ec3c5a4345f97ecc92
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-09-01 15:27:16 -0400

    created login/signup page and logout button

--- server.js
+++ server.js
@@ -1,16 +1,28 @@
 const path = require('path');
 const express = require('express');
 const routes = require('./controllers');
-const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
+const session = require('express-session');
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const hbs = exphbs.create({});
 const app = express();
 const PORT = process.env.PORT || 3001;
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
